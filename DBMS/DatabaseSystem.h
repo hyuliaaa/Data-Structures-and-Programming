ee32b35445d711f90ae69fa1b30e8f337cbc22eb
hyuliaaa <54545875+hyuliaaa@users.noreply.github.com>
2021-01-11 18:17:46 +0200

    Update DatabaseSystem.h

--- DBMS/DatabaseSystem.h
+++ DBMS/DatabaseSystem.h
@@ -5,6 +5,7 @@
 #ifndef DBMS_DATABASESYSTEM_H
 #define DBMS_DATABASESYSTEM_H
 
+#include <list>
 #include "Table.h"
 
 class DatabaseSystem {
@@ -12,10 +13,18 @@ private:
     std::vector<Table> db;
 public:
 
-    void createTable(const std::string& name); //creates new table
-    void insertRow(const std::string& name); //inserts row in a table with tablename name
-
+    bool createTable(const std::string& name); //creates new table ; poradi nqkakva prichina ne moga da syzdam 2 tablici
+    void addColumn(const std::string& name,const std::string& colName, int type, bool hasPrimaryKey);
+    void addColumn(const std::string& name, Column *c);
+    void insertInto(const std::string& name); //inserts row in a table with tablename name
+    bool findTable(const std::string& name); //finds table with given name
+    void printTableNames();//prints all table names found in the database
+    int getSizeofDataBase();
+    void printTableWithName(const std::string& name);
+    
+   
 };
 
 
 #endif //DBMS_DATABASESYSTEM_H
+
